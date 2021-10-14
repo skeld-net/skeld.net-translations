90d2f23d43e4b3369c5adfa661676ed7c234116c
danbudworthmead <danbudworthmead@gmail.com>
2021-10-14 16:59:46 +0100

    Initial commit

---
+++ english.ts
@@ -0,0 +1,11 @@
+import { Translations } from "../Translations";
+
+export const EnglishTranslations: Translations = {
+    WelcomeName: "Welcome to skeld.net",
+    WelcomeMessage: `
+Worlds first custom Among Us server
+Type /help to get started
+Current gamemode: @{gamemode[0]}
+
+Subscribe to our Patreon https://www.patreon.com/skeld_net to get a coloured in-game name!`.trim(),
+};
---
+++ french.ts
@@ -0,0 +1,11 @@
+import { Translations } from "../Translations";
+
+export const FrenchTranslations: Translations = {
+    WelcomeName: "Bienvenue sur skeld.net",
+    WelcomeMessage: `
+Premier serveur parmi nous personnalisé au monde
+Tapez /help pour commencer
+Mode de jeu actuel : @{gamemode[0]}
+
+Abonnez-vous à notre Patreon https://www.patreon.com/skeld_net pour obtenir un nom coloré dans le jeu !`.trim(),
+};
