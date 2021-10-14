193ddab1f47b442965f157050c5b0466a9a4247c
danbudworthmead <danbudworthmead@gmail.com>
2021-10-14 17:01:21 +0100

    Create italian.ts

---
+++ italian.ts
@@ -0,0 +1,11 @@
+import { Translations } from "../Translations";
+
+export const ItalianTranslations: Translations = {
+    WelcomeName: "Benvenuto su skeld.net",
+    WelcomeMessage: `
+Il primo server personalizzato Among Us al mondo
+Digita /help per iniziare
+Modalità di gioco attuale: @{gamemode[0]}
+
+Iscriviti al nostro Patreon https://www.patreon.com/skeld_net per ottenere un nome di gioco colorato!`.trim(),
+};
