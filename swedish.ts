5dbb2b8d93132b8cbf02f2a57b97959d685f0f54
The Captain <81252038+Captain8771@users.noreply.github.com>
2021-10-23 18:10:33 +0200

    swedish, also fix a small typo in english.ts, where `and` should be used instead of `to`

---
+++ swedish.ts
@@ -0,0 +1,13 @@
+import { Translations } from "..";
+
+export const SwedishTranslations: Translations = {
+    WelcomeName: "Välkommen till skeld.net",
+    WelcomeMessage: `
+Världens första inofficiella among us server
+Skriv /help för att börja
+Valt spelläge: @{gamemode[0]}
+
+Prenumerera till vår Patreon https://www.patreon.com/skeld_net för att få ett färgat namn i spelet!`.trim(),
+    SetImpostorsInvalidCount:
+        "ogiltig spelarmängd, snälla skriv ett nummer mellan 1 och 3",
+};
