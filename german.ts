185897f4159f28d8df1f76caf97492570673e99a
EnderB0YHD <edwearda@outlook.de>
2021-10-23 17:58:02 +0200

    Added SetImpostorsInvalidCount for german

--- german.ts
+++ german.ts
@@ -8,4 +8,7 @@ export const GermanTranslations: Translations = {
         Aktueller Spielmodus: @{gamemode[0]}
         
         Abonniere unseren Patreon https://www.patreon.com/skeld_net, um einen farbigen Namen im Spiel zu erhalten!`.trim(),
+
+    SetImpostorsInvalidCount:
+        "Ungültige Spieler Anzahl, bitte geben Sie eine Nummer zwischen 1 und 3 ein",
 };
