32fa06513be6a4216e6c1dd4cc3061708e8688e0
NorwayLCAndTrains <93440975+NorwayLCAndTrains@users.noreply.github.com>
2021-10-30 17:29:43 +0200

    Create norwegian.ts

---
+++ norwegian.ts
@@ -0,0 +1,25 @@
+import { Translations } from "..";
+
+export const EnglishTranslations: Translations = {
+    WelcomeName: "Velkommen til Skeld.net",
+    WelcomeMessage: `
+Verdens første tilpasset Among Us server
+Skriv /help til å starte
+Gamemode akkurat nå: @{gamemode[0]}
+
+Abonner til vår Patreon https://www.patreon.com/skeld_net til å få ett farge i-game navn!`.trim(),
+    SetImpostorsInvalidCount:
+        "Ugyldig spill nummer, please enter a number between 1 and 3",
+    SetImpostorsSuccess: "Impostor nummer ble settet til @{number[0]}",
+    ZombiesIncomingIn: "Zombies skal komme i @{number[0]} seconds",
+    GamemodeChangedTo: "Gamemode byttet til @{gamemode[0]}",
+    SquidGameLightsInstructions:
+        "Trykk på møte knappen før tiden går tom",
+    SquidGameDalgonaInstructions:
+        "Bli ferdig med dine oppgaver får tiden går tom",
+    SquidGameTugOfWarInstructions:
+        "Gå i det riktige retningen til å dra det andre laget",
+    StartingIn: "Starter i ",
+    MaxPlayersModeSwitch:
+        "Verten byttet moden og nå er dette rommet fullt",
+};
