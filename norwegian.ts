4278f09a4f5e0699ba0368ab2e24b1d028e442d9
danbudworthmead <danbudworthmead@gmail.com>
2021-10-30 17:06:59 +0100

    Update norwegian.ts

--- norwegian.ts
+++ norwegian.ts
@@ -1,6 +1,6 @@
 import { Translations } from "..";
 
-export const EnglishTranslations: Translations = {
+export const NorwegianTranslations: Translations = {
     WelcomeName: "Velkommen til Skeld.net",
     WelcomeMessage: `
 Verdens første tilpasset Among Us server
@@ -13,13 +13,11 @@ Abonner til vår Patreon https://www.patreon.com/skeld_net til å få ett farge 
     SetImpostorsSuccess: "Impostor nummer ble settet til @{number[0]}",
     ZombiesIncomingIn: "Zombies skal komme i @{number[0]} seconds",
     GamemodeChangedTo: "Gamemode byttet til @{gamemode[0]}",
-    SquidGameLightsInstructions:
-        "Trykk på møte knappen før tiden går tom",
+    SquidGameLightsInstructions: "Trykk på møte knappen før tiden går tom",
     SquidGameDalgonaInstructions:
         "Bli ferdig med dine oppgaver får tiden går tom",
     SquidGameTugOfWarInstructions:
         "Gå i det riktige retningen til å dra det andre laget",
     StartingIn: "Starter i ",
-    MaxPlayersModeSwitch:
-        "Verten byttet moden og nå er dette rommet fullt",
+    MaxPlayersModeSwitch: "Verten byttet moden og nå er dette rommet fullt",
 };
