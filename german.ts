6be11b7b8210b8bb6a01ebeb779628efa57223d8
EnderB0YHD <edwearda@outlook.de>
2021-11-02 15:35:06 +0100

    Added some translation for german

--- german.ts
+++ german.ts
@@ -13,4 +13,30 @@ export const GermanTranslations: Translations = {
         "Ungültige Spieler Anzahl, bitte geben Sie eine Nummer zwischen 1 und 3 ein",
     SetImpostorsSuccess:
         "Impostor Anzahl wurde auf @{number[0]} gesetzt",
+    ZombiesIncomingIn:
+        "Zombies erscheinen in @{number[0]} Sekunden",
+    GamemodeChangedTo:
+        "Spielmodus wurde zu @{gamemode[0]} geändert",
+    SquidGameLightsInstructions:
+        "Drücke auf den Treffknopf, bevor die Zeit umgeht",
+    SquidGameDalgonaInstructions:
+        "Beende alle deiner Aufgaben, bevor die Zeit umgeht",
+    SquidGameTugOfWarInstructions:
+        "Bewege in die richtige Richtung um das gegnerische Team zu ziehen",
+    StartingIn:
+        "Startet in: ",
+    MaxPlayersModeSwitch:
+        "Der Gastgeber wechselte den Spielmodus und nun ist der Raum voll",
+    InvalidMap:
+        "Ungültige Karte. Verfügbare Karten: ",
+    MapChanged:
+        "Karte wurde gesetzt auf: ",
+    InvalidColour:
+        "Ungültige Farbe. Verfügbare Farben: ",
+    GamemodeDoesntExist:
+        "Spielmodus @{text[0]} existiert nicht!",
+    InvalidMaxPlayers:
+        "Bitte gebe eine gültige Spieleranzahl ein (eine Zahl von 4 bis @{number[0]})",
+    MaxPlayersChanged:
+        "Maximale Spieleranzahl ist nun @{text[0]}"
 };
